c672c055ce889b42938986823a47ca9f8a68328d
duomamet <DUOMAMET@GMAIL.COM>
2025-04-28 21:29:52 +0700

    Create song.js

---
+++ song.js
@@ -0,0 +1,18 @@
+// songs.js
+const songs = [
+  {
+    title: "Berpayung Tuhan",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Nadin%20Amizah%20-%20Berpayung%20Tuhan%20(Official%20Lyric%20Video).mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/Nadin%20amizah.jpg"
+  },
+  {
+    title: "Tapi Diterima",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Nadin%20Amizah%20-%20Tapi%20Diterima%20(Official%20Lyric%20Video).mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/Nadin%20amizah.jpg"
+  },
+  {
+    title: "Lagu 3",
+    file: "audio3.mp3",
+    cover: "cover3.jpg"
+  }
+];
