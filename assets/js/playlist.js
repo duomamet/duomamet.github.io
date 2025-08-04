fbbcc1b456815117ec4c8226a7de631cfae2898b
duomamet <DUOMAMET@GMAIL.COM>
2025-08-04 11:09:33 +0700

    Create playlist.js

---
+++ assets/js/playlist.js
@@ -0,0 +1,63 @@
+ // playlist.js
+export const songs = [
+  {
+    title: "Jikalau",
+    artist: "Naff",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Jikalau.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273469bd0bb3f9cf222cdaf2e2d"
+  },
+  {
+    title: "Tapi Diterima",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Nadin%20Amizah%20-%20Tapi%20Diterima%20(Official%20Lyric%20Video).mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/Nadin%20amizah.jpg"
+  },
+  {
+    title: "Forgot Password",
+    artist: "Hindia ft. Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Hindia%20ft.%20Nadin%20Amizah%20-%20Forgot%20Password%20(Official%20Lyric%20Video).mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/hindianadin.jpg"
+  },
+  {
+    title: "Gagal Menjadi Manusia",
+    artist: "Tulus",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Tulus%20-%20Gagal%20Menjadi%20Manusia.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/tulus.jpg"
+  },
+  {
+    title: "Lagu Untukmu",
+    artist: "Nidji",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Nidji%20-%20Lagu%20Untukmu.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/nidji.jpg"
+  },
+  {
+    title: "Kita Bisa Bersama",
+    artist: "Tulus",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Tulus%20-%20Kita%20Bisa%20Bersama.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/tulus.jpg"
+  },
+  {
+    title: "Akhirnya Ku Menemukanmu",
+    artist: "Anji",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Anji%20-%20Akhirnya%20Ku%20Menemukanmu.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/anji.jpg"
+  },
+  {
+    title: "Sampai Tua Nanti",
+    artist: "Gigi",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Gigi%20-%20Sampai%20Tua%20Nanti.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/gigi.jpg"
+  },
+  {
+    title: "Pergi Pagi Pulang Pagi",
+    artist: "Slank",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Slank%20-%20Pergi%20Pagi%20Pulang%20Pagi.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/slank.jpg"
+  },
+  {
+    title: "Laskar Pelangi",
+    artist: "Radja",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Radja%20-%20Laskar%20Pelangi.mp3",
+    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/radja.jpg"
+  }
+];
