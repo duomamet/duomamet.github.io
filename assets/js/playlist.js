083d5d8d0a4e7c3dd147f25a67a7311ebae7b473
duomamet <DUOMAMET@GMAIL.COM>
2025-08-04 11:49:00 +0700

    Update playlist.js

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -1,63 +1,152 @@
- // playlist.js
 export const songs = [
   {
-    title: "Jikalau",
-    artist: "Naff",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Jikalau.mp3",
-    cover: "https://i.scdn.co/image/ab67616d0000b273469bd0bb3f9cf222cdaf2e2d"
-  },
-  {
-    title: "Tapi Diterima",
+    title: "Taruh",
     artist: "Nadin Amizah",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Nadin%20Amizah%20-%20Tapi%20Diterima%20(Official%20Lyric%20Video).mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/Nadin%20amizah.jpg"
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Taruh.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/1d/10/9b/1d109b17-1bbf-1894-e774-4d9ad6baa7b3/cover.jpg/500x500bb.jpg"
   },
   {
-    title: "Forgot Password",
-    artist: "Hindia ft. Nadin Amizah",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Hindia%20ft.%20Nadin%20Amizah%20-%20Forgot%20Password%20(Official%20Lyric%20Video).mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/hindianadin.jpg"
+    title: "Tawa",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Tawa.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/1c/5f/76/1c5f761f-6ac4-12ea-157b-1386f8f46076/3663729076076_cover.jpg/500x500bb.jpg"
   },
   {
-    title: "Gagal Menjadi Manusia",
+    title: "33x",
+    artist: "Perunggu",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/33x.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/74/f6/0c/74f60cb4-2f04-eb97-7047-bdb07747140f/cover.jpg/592x592bb.webp"
+  },
+  {
+    title: "2112",
+    artist: "Reality Club",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/2112.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/a7/0c/03/a70c0335-d4ee-2bca-a724-d605508a4457/23UMGIM51089.rgb.jpg/600x600bf-60.jpg"
+  },
+  {
+    title: "123456",
+    artist: "Budi Doremi",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/123456.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/79/54/71/795471ea-6a76-f24a-07af-a4b998f7310c/123456_-_Budi_Doremi.jpg/600x600bf-60.jpg"
+  },
+  {
+    title: "Ah",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Ah.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/1c/5f/76/1c5f761f-6ac4-12ea-157b-1386f8f46076/3663729076076_cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Alexandra",
+    artist: "Nikita",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Alexandra.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/0f/40/35/0f4035b8-eca1-2f98-3728-1a7016f67edc/24UMGIM04757.rgb.jpg/592x592bb.webp"
+  },
+  {
+    title: "Bertaut",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Bertaut.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/1d/10/9b/1d109b17-1bbf-1894-e774-4d9ad6baa7b3/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Cermin",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Cermin.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/1d/10/9b/1d109b17-1bbf-1894-e774-4d9ad6baa7b3/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Ekspektasi",
+    artist: "Okin & Ocan Siagian",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Ekspektasi.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/68/93/1b/68931bcf-36c2-5f45-1d24-1eddb69b33cb/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Fakboi",
+    artist: "Okin & Ocan Siagian    ",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Fakboi.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music113/v4/68/93/1b/68931bcf-36c2-5f45-1d24-1eddb69b33cb/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Gaze",
+    artist: "Adhitia Sofyan",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Gaze.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/6e/fd/99/6efd9916-18ff-17f2-5170-6d1a69cc4163/3800152026074.png/592x592bb.webp"
+  },
+  {
+    title: "Jikalau",
+    artist: "Naif",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Jikalau.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/49/96/6e/49966e2b-e03f-63de-a562-04e004e0cc3e/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Kekal",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Kekal.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/d5/3c/16/d53c1625-bdbc-b463-c309-5b0de8fde1f6/cover.jpg/592x592bb.webp"
+  },
+  {
+    title: "Kuning",
+    artist: "rumahsakit",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Kuning.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/29/54/aa/2954aac7-887f-50d4-9023-7f3d83a81adc/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "L",
+    artist: "Hal",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/L.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/d4/d8/fb/d4d8fb12-19d6-5087-2ea2-840a038d8eb4/cover.jpg/592x592bb.webp"
+  },
+  {
+    title: "Melting",
+    artist: "Kali Uchis",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Melting.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/76/0a/43/760a4316-b2c9-ab54-3e23-cb927d1dd352/859716530826_cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Mendarah",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Mendarah.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/1d/10/9b/1d109b17-1bbf-1894-e774-4d9ad6baa7b3/cover.jpg/500x500bb.jpg"
+  },
+  {
+    title: "Monokrom",
     artist: "Tulus",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Tulus%20-%20Gagal%20Menjadi%20Manusia.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/tulus.jpg"
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Monokrom.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music18/v4/d1/8f/41/d18f41cf-45e4-39f4-4d10-0a0af01f7eb7/cover.jpg/500x500bb.jpg"
   },
   {
-    title: "Lagu Untukmu",
-    artist: "Nidji",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Nidji%20-%20Lagu%20Untukmu.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/nidji.jpg"
+    title: "Nadir",
+    artist: "Fiersa Besari",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Nadir.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/bc/fe/56/bcfe568c-c26c-3387-0cf0-201f045cfde3/3616402872533.png/592x592bb.webp"
   },
   {
-    title: "Kita Bisa Bersama",
-    artist: "Tulus",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Tulus%20-%20Kita%20Bisa%20Bersama.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/tulus.jpg"
+    title: "Panasea",
+    artist: "rumahsakit",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Panasea.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/f8/fc/2f/f8fc2fa8-1035-63c5-d8d3-6678452711d8/575.jpg/500x500bb.jpg"
   },
   {
-    title: "Akhirnya Ku Menemukanmu",
-    artist: "Anji",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Anji%20-%20Akhirnya%20Ku%20Menemukanmu.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/anji.jpg"
+    title: "Rumpang",
+    artist: "Nadin Amizah",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Rumpang.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music126/v4/4f/d3/55/4fd355ba-f0e8-b035-d424-fc29e6f7863e/cover.jpg/500x500bb.jpg"
   },
   {
-    title: "Sampai Tua Nanti",
-    artist: "Gigi",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Gigi%20-%20Sampai%20Tua%20Nanti.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/gigi.jpg"
+    title: "Seventeen",
+    artist: "Barrett Wilbert Weed & Ryan McCartan",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Seventeen.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music114/v4/4f/8b/23/4f8b2380-ae9e-e317-b3c7-f55afcfff051/859712752819_cover.jpg/500x500bb.jpg"
   },
   {
-    title: "Pergi Pagi Pulang Pagi",
-    artist: "Slank",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Slank%20-%20Pergi%20Pagi%20Pulang%20Pagi.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/slank.jpg"
+    title: "Sorai",
+    artist: "NEMOPHILA",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Sorai.mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/5e/db/1a/5edb1a1f-389b-09c5-2004-c44f310e2cb7/794558907064.jpg/500x500bb.jpg"
   },
   {
-    title: "Laskar Pelangi",
-    artist: "Radja",
-    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/Radja%20-%20Laskar%20Pelangi.mp3",
-    cover: "https://raw.githubusercontent.com/duomamet/duomamet.github.io/refs/heads/main/radja.jpg"
+    title: "Sssst...",
+    artist: "Sore",
+    file: "https://github.com/duomamet/duomamet.github.io/raw/refs/heads/main/song/Sssst....mp3",
+    cover: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/d3/9d/d3/d39dd3ac-77ba-e392-5d50-9e4171aff8d7/8721215887362.png/500x500bb.jpg"
   }
 ];
